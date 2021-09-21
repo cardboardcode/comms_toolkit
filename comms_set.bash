7bb9be5f1a044d556c286f86d0e26c8a20fb6729
Bey Hao Yun <Bey_Hao_Yun@artc.a-star.edu.sg>
2021-09-21 18:31:00 +0800

    :heavy_plus_sign: Added more basic aliases in comms_set.bash.

--- comms_set.bash
+++ comms_set.bash
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+
+COLUMNS=$(tput cols)
+title="[-WELCOME-]"
+printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+
 # General
 alias b='cd ..'
+alias d='exit'
+alias cs='clear'
+alias bb='cd -'
+alias nautilus='nautilus >/dev/null 2>&1'
+alias open='nautilus .'
 alias admin='nano ~/.bashrc -l --mouse'
 alias rest='source ~/.bashrc'
 
